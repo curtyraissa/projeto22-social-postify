87b85c75aede9e91d6c18a9706027531b7a29e7f
curtyraissa <raissacurty@gmail.com>
2023-08-23 23:06:36 +0100

    correcao prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MediasModule } from './medias/medias.module';
+import { PrismaService } from './prisma/prisma.service';
+import { PrismaModule } from './prisma/prisma.module';
 import { PublicationsModule } from './publications/publications.module';
 import { PostsModule } from './posts/posts.module';
 import { PostsModule } from './posts/posts.module';
@@ -9,8 +11,8 @@ import { PublicationsModule } from './publications/publications.module';
 import { MediasModule } from './medias/medias.module';
 
 @Module({
-  imports: [MediasModule, PostsModule, PublicationsModule],
+  imports: [MediasModule, PostsModule, PublicationsModule, PrismaModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
