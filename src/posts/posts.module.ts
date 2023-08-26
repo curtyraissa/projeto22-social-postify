f96432df3c5808e82c5322268ed1b8cd1c0c19d0
curtyraissa <raissacurty@gmail.com>
2023-08-26 18:00:44 +0100

    correcao post delete module

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,13 +1,14 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { PostService } from './posts.service';
 import { PostsController } from './posts.controller';
 import { PrismaModule } from '../prisma/prisma.module';
 import { PostRepository } from './posts.repository';
+import { PublicationModule } from '../publications/publications.module';
 
 @Module({
   controllers: [PostsController],
   providers: [PostService, PostRepository],
   exports: [PostService],
-  imports: [PrismaModule],
+  imports: [PrismaModule, forwardRef(()=> PublicationModule)],
 })
 export class PostsModule {}
